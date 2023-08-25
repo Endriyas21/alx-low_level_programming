b2d043f51ca0e3d8f592424957fb27c837ae2453
Endriyas <endriyasbi@uni.minerva.edu>
2023-08-24 17:21:24 -0700

    corr

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,21 +1,13 @@
 #include <stdio.h>
 
-/**
- * before_main - Prints "You're beat! and yet, you must allow,"
- *              and "I bore my house upon my back!" before main.
- */
-void __attribute__((constructor)) before_main(void)
-{
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
+void print_before_main(void) __attribute__ ((constructor));
 
 /**
- * main - Entry point of the program.
- *
- * Return: Always 0.
+ * print_before_main - prints a sentence before the main
+ * function is executed
  */
-int main(void)
+void print_before_main(void)
 {
-    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-    return (0);
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
