e1084f9d4016cd6b563a16dd869160f4491a5593
Endriyas <endriyasbi@uni.minerva.edu>
2023-08-29 17:26:46 -0700

    final1

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,21 +2,21 @@
 
 /**
  * reverse_listint - Reverses a listint_t linked list.
- * @head: Double pointer to the head of the linked list.
- * Return: Pointer to the first node of the reversed list.
+ * @head: Pointer to the pointer of the head of the linked list.
+ *
+ * Return: Pointer to the new head of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
     listint_t *prev = NULL;
-    listint_t *current = *head;
-    listint_t *next;
+    listint_t *next = NULL;
 
-    while (current != NULL)
+    while (*head)
     {
-        next = current->next;
-        current->next = prev;
-        prev = current;
-        current = next;
+        next = (*head)->next;
+        (*head)->next = prev;
+        prev = *head;
+        *head = next;
     }
 
     *head = prev;
--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,36 +1,88 @@
-#include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
+
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
 
 /**
- * print_listint_safe - Prints a listint_t linked list safely.
- * @head: Pointer to the head of the linked list.
- * Return: Number of nodes in the list.
+ * looped_listint_len - Counts the number of unique nodes
+ * in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ * Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	const listint_t *slow_runner, *fast_runner;
+	size_t unique_nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	slow_runner = head->next;
+	fast_runner = (head->next)->next;
+
+	while (fast_runner)
+	{
+		if (slow_runner == fast_runner)
+		{
+			slow_runner = head;
+			while (slow_runner != fast_runner)
+			{
+				unique_nodes++;
+				slow_runner = slow_runner->next;
+				fast_runner = fast_runner->next;
+			}
+
+			slow_runner = slow_runner->next;
+			while (slow_runner != fast_runner)
+			{
+				unique_nodes++;
+				slow_runner = slow_runner->next;
+			}
+
+			return (unique_nodes);
+		}
+
+		slow_runner = slow_runner->next;
+		fast_runner = (fast_runner->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a listint_t list safely.
+ * @head: A pointer to the head of the listint_t list.
+ *
+ * Return: The number of nodes in the list.
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *current = head;
-    const listint_t *check_loop = head;
+	size_t nodes, index = 0;
 
-    while (check_loop && check_loop->next)
-    {
-        check_loop = check_loop->next->next;
-        if (check_loop == current)
-        {
-            printf("-> [%p] %d\n", (void *)current, current->n);
-            exit(98);
-        }
-        current = current->next;
-    }
+	nodes = looped_listint_len(head);
 
-    current = head;
-    while (current)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        current = current->next;
-        count++;
-    }
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
 
-    return count;
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
 }
+
